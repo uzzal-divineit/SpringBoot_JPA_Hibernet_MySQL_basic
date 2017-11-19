ec7623894a71c5abedf6258db42a2223e86e0ed6
uzzl-divineit <uzzal@divine-it.net>
2017-11-19 12:08:27 +0600

    add more functanility

--- src/main/java/net/divineit/uzzal/SpringBoot_JPA_Hibernet_basic/repository/NoteRepository.java
+++ src/main/java/net/divineit/uzzal/SpringBoot_JPA_Hibernet_basic/repository/NoteRepository.java
@@ -5,7 +5,11 @@ import net.divineit.uzzal.SpringBoot_JPA_Hibernet_basic.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
+    List<Note> getSimillerNote(String tag);
+    List<Note> getId();
 }
